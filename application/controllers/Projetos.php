60b95e3f6d1caa374ff45c753ba197f8c4b70a28
patrick-moreira <patrickmoreira1995@gmail.com>
2019-01-16 19:24:35 -0200

    Backend da página Projetos

--- application/controllers/Projetos.php
+++ application/controllers/Projetos.php
@@ -3,11 +3,14 @@
     class Projetos extends CI_Controller {
         public function __construct(){
             parent::__construct();
+
+            $this->load->model('projetos_model','modelprojetos');
         }
         
-        public function index()
-        {
-            $this->load->view('frontend/template/html-header');
+        public function index(){
+            $dados['listaprojetos'] = $this->modelprojetos->listar_projetos();
+            
+            $this->load->view('frontend/template/html-header', $dados);
             $this->load->view('frontend/template/header');
             $this->load-> view('frontend/projetos'); //carrega o arquivo projetos dentro da pasta view
             $this->load->view('frontend/template/footer');
---
+++ application/models/Projetos_model.php
@@ -0,0 +1,42 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Projetos_model extends CI_Model {
+        public $id;
+        public $titulo;
+        public $descricao;
+
+        public function __construct(){
+            parent::__construct();
+        }
+
+        public function listar_projetos(){
+            $this->db->order_by('titulo','ASC');
+            return $this->db->get('projeto')->result();
+        }
+
+        public function listar_projeto($id){
+            $this->db->from('projeto');
+            $this->db->where('md5(id)',$id);
+            return $this->db->get()->result();
+        }
+
+        public function adicionar($titulo, $descricao){
+            $dados['titulo'] = $titulo;
+            $dados['descricao'] = $descricao;
+            return $this->db->insert('projeto',$dados); 
+        }
+
+        public function excluir($id){
+            $this->db->where('md5(id)', $id);
+            return $this->db->delete('projeto');
+        }
+
+        public function alterar($id, $titulo, $descricao){
+            $dados['titulo'] = $titulo;
+            $dados['descricao'] = $descricao;
+            $this->db->where('id',$id);
+            return $this->db->update('projeto', $dados);
+        }
+    }   
+?>
