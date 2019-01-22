407d24fa85844b27b8525de475a315d065351e42
rogerd ribeiro <rogerdribeiro@gmail.com>
2019-01-22 17:55:39 -0200

    frontend pagina de eventos

---
+++ application/views/frontend/eventos.php
@@ -0,0 +1,51 @@
+<div id="eventos" style="
+    padding-right: 10%;
+    padding-left: 10%;
+    padding-top: 5%;
+    padding-bottom: 5%;">
+
+    <h2 class="subtitle" style="ont-size: 30px;
+    color: #222;
+    margin-bottom: 20px;
+    margin-top: 40px;">Eventos</h2>
+    <div class="anos">
+        <ul>
+            <li><a href="#">2019</a></li>
+            <li><a href="#">2018</a></li>
+            <li><a href="#">2017</a></li>
+            <li><a href="#">2016</a></li>
+            <li><a href="#">2015</a></li>
+            <li><a href="#">2014</a></li>
+            <li><a href="#">2013</a></li>
+            <li><a href="#">2012</a></li>
+            <li><a href="#">2011</a></li>
+            <li><a href="#">2010</a></li>
+            <li><a href="#">2009</a></li>
+        </ul>
+    </div>
+    <h2 class="anoTxt">2019</h2>
+
+    <div class="evento">
+        <h2>Evento 1</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <center>
+            <img style="    margin-top: 10px;" src="http://adoracaoevida.com/www/wp-content/gallery/eu-fui_1/foto_teste_2.jpg" />
+            <img style="    margin-top: 10px;" src="http://adoracaoevida.com/www/wp-content/gallery/eu-fui_1/foto_teste_2.jpg" />
+            <img style="    margin-top: 10px;" src="http://adoracaoevida.com/www/wp-content/gallery/eu-fui_1/foto_teste_2.jpg" />
+        </center>
+        <hr>
+
+
+        <h2>Evento 2</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <center>
+            <img style="    margin-top: 10px;" src="http://adoracaoevida.com/www/wp-content/gallery/eu-fui_1/foto_teste_2.jpg" />
+            <img style="    margin-top: 10px;" src="http://adoracaoevida.com/www/wp-content/gallery/eu-fui_1/foto_teste_2.jpg" />
+        </center>
+        <hr>
+
+    </div>
+    
+
+</div>
+
