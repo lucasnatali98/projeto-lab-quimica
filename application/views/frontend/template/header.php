64fe1841b8a086cdfa6e708f3bbff0eff9fdba88
Joaquim Júnior <jojuvi3@gmail.com>
2019-01-28 10:44:08 -0200

    navbar

--- application/views/frontend/template/header.php
+++ application/views/frontend/template/header.php
@@ -1,22 +1,5 @@
 <body>
 
-	<!-- <nav class="fixed-top navbar-light bg-white border border-secondary" >
-				
-			<ul class = "nav top-fixed navbar-nav navbar-right  bg-white" style="margin-left:2%;">
-					<li class="nav-item"><a href="<?php echo base_url() ?>"><img src="<?php echo base_url('/assets/frontend/img/gepcec_logotipo.png'); ?>" class="logo" ></a></li>
-					<li class="nav-item active"><a class ="nounderline" href="<?php echo base_url() ?>">Home</a></li>
-					<li class="nav-item"><a class ="nounderline" href="<?php echo base_url('/projetos') ?>">Projetos</a></li>
-					<li class="nav-item"><a class ="nounderline" href="<?php echo base_url() ?>">Produções</a></li>
-					<li class="nav-item"><a class ="nounderline" href="<?php echo base_url() ?>">Eventos</a></li>
-					<li class="nav-item"><a class ="nounderline" href="<?php echo base_url() ?>">Galeria</a></li>
-					<li class="nav-item active"><a class ="nounderline" href="<?php echo base_url('/pessoal') ?>">Pessoal</a></li>
-					<li class="nav-item"><a class ="nounderline" href="<?php echo base_url() ?>">Contatos</a></li>
-							
-			</ul>		
-			
-
-
-	</nav> -->
 	<div class="container ">
 		<div class="row">
 			<div class="col-12">
