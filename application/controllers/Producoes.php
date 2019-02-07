281312fc2fc4ea134cfacfc7596f727ce309a127
rogerd ribeiro <rogerdribeiro@gmail.com>
2019-02-07 16:13:33 -0200

    frontend producoes

--- application/controllers/Producoes.php
+++ application/controllers/Producoes.php
@@ -3,10 +3,15 @@
     class Producoes extends CI_Controller {
         public function __construct(){
             parent::__construct();
+            $this->load->model('producoes_model','modelproducoes');
+
         }
         public function index()
         {
-            $this->load->view('frontend/template/html-header');
+            $dados['listaproducoes'] = $this->modelproducoes->listar_producoes();
+            $dados['listaDeAnos'] = $this->modelproducoes->listar_anos();
+
+            $this->load->view('frontend/template/html-header',$dados);
             $this->load->view('frontend/template/header');
             $this->load-> view('frontend/producoes'); //carrega o arquivo producoes dentro da pasta view
             $this->load->view('frontend/template/footer');
--- application/views/frontend/producoes.php
+++ application/views/frontend/producoes.php
@@ -1,141 +1,52 @@
 <div id="producoes">
   <h2 class="subtitle">Produções</h2>
+
   <div class="anos">
-      <ul>
-        <li> <a href="#">2019 </a></li>
-        <li> <a href="#">2018</a></li>
-        <li> <a href="#">2017</a></li>
-        <li> <a href="#">2016</a></li>
-        <li> <a href="#">2015</a></li>
-        <li> <a href="#">2014</a></li>
-        <li> <a href="#">2013</a></li>
-        <li> <a href="#">2012</a></li>
-        <li> <a href="#">2011</a></li>
-        <li> <a href="#">2010</a></li>
-        <li> <a href="#">2009</a></li>
-
-      </ul>
-  </div>
-  <h4 class="anoTxt">2019</h4>
-
-  <!--Accordion wrapper-->
-<div class="accordion md-accordion" id="accordionEx" role="tablist" aria-multiselectable="true">
-
-<!-- Accordion card -->
-<div class="card">
-
-  <!-- Card header -->
-  <div class="card-header" role="tab" id="headingOne1">
-    <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne1" aria-expanded="true"
-      aria-controls="collapseOne1">
-      <h5 class="mb-0">
-      Produção 1 <i class="fa fa-angle-down rotate-icon"></i>
-      </h5>
-    </a>
+    <ul>
+      <li>
+        <?php foreach($listaDeAnos as $ano){?>
+          <a href="#<?php echo $ano ?>"><?php echo $ano ?> | </a> 
+        <?php }?>
+      </li>
+    </ul>
   </div>
 
-  <!-- Card body -->
-  <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordionEx">
-    <div class="card-body">
-      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-      labore sustainable VHS.
-      <br>
-      <div class="media-div">
-        <img src="<?php echo base_url('/assets/frontend/img/logo1.jpg'); ?>" />
-        <img src="<?php echo base_url('/assets/frontend/img/logo2.jpg'); ?>" />
-        <img src="<?php echo base_url('/assets/frontend/img/logo3.jpg'); ?>" />
-        
-        <button class="btn-galeria"> Abrir </button>
-      </div>
-      
-    </div>
+  <div class="accordion md-accordion" id="accordionEx" role="tablist" aria-multiselectable="true">
+
+    <?php foreach($listaDeAnos as $ano){?>
+      <h2 id="<?php echo $ano ?>" class="anoTxt"><?php echo $ano ?></h2> 
+
+      <?php $i=0; foreach($listaproducoes as $producao) {
+        if($producao->ano == $ano){?>              
+
+          <div class="card">
+            <div class="card-header" role="tab" id="headingOne1">
+              <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne1" aria-expanded="true" aria-controls="collapseOne1">
+                <h5 class="mb-0"> Titulo 
+                  <i class="fa fa-angle-down rotate-icon"></i>
+                </h5>
+              </a>
+            </div>
+
+            <!-- Card body -->
+            <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordionEx">
+              <div class="card-body">
+                Descrição
+                <br>
+                <div class="media-div">
+                  <img src="<?php echo base_url('/assets/frontend/img/logo1.jpg'); ?>" />
+                  <img src="<?php echo base_url('/assets/frontend/img/logo2.jpg'); ?>" />
+                  <img src="<?php echo base_url('/assets/frontend/img/logo3.jpg'); ?>" />
+                  
+                  <button class="btn-galeria"> Abrir </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php $i++; }
+      }
+    }?>
   </div>
-
-</div>
-<!-- Accordion card -->
-
-<!-- Accordion card -->
-<div class="card">
-
-  <!-- Card header -->
-  <div class="card-header" role="tab" id="headingTwo2">
-    <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2"
-      aria-expanded="false" aria-controls="collapseTwo2">
-      <h5 class="mb-0">
-      Produção 2 <i class="fa fa-angle-down rotate-icon"></i>
-      </h5>
-    </a>
-    
-  </div>
-
-  <!-- Card body -->
-  <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2" data-parent="#accordionEx">
-    <div class="card-body">
-      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-      labore sustainable VHS.
-      <br>
-      <div class="media-div">
-        <img src="<?php echo base_url('/assets/frontend/img/logo1.jpg'); ?>" />
-        <img src="<?php echo base_url('/assets/frontend/img/logo2.jpg'); ?>" />
-        <img src="<?php echo base_url('/assets/frontend/img/logo3.jpg'); ?>" />
-        
-        <button class="btn-galeria"> Abrir </button>
-      </div>
-    </div>
-  </div>
-
-</div>
-<!-- Accordion card -->
-
-<!-- Accordion card -->
-<div class="card">
-
-  <!-- Card header -->
-  <div class="card-header" role="tab" id="headingThree3">
-    <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseThree3"
-      aria-expanded="false" aria-controls="collapseThree3">
-      <h5 class="mb-0">
-      Produção 3 <i class="fa fa-angle-down rotate-icon"></i>
-      </h5>
-    </a>
-  </div>
-
-  <!-- Card body -->
-  <div id="collapseThree3" class="collapse" role="tabpanel" aria-labelledby="headingThree3" data-parent="#accordionEx">
-    <div class="card-body">
-      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-      wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
-      eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-      nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
-      farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
-      labore sustainable VHS.
-      <br>
-      <div class="media-div">
-        <img src="<?php echo base_url('/assets/frontend/img/logo1.jpg'); ?>" />
-        <img src="<?php echo base_url('/assets/frontend/img/logo2.jpg'); ?>" />
-        <img src="<?php echo base_url('/assets/frontend/img/logo3.jpg'); ?>" />
-        
-        <button class="btn-galeria"> Abrir </button>
-      </div>
-    </div>
-  </div>
-
-</div>
-<!-- Accordion card -->
-
-</div>
 <!-- Accordion wrapper -->
-
 </div>
 
