b88b81a7bb8149c4fe184ea9789319542f5260d2
rogerd ribeiro <rogerdribeiro@gmail.com>
2019-02-07 16:18:41 -0200

    frontend producoes

--- application/views/frontend/producoes.php
+++ application/views/frontend/producoes.php
@@ -22,7 +22,7 @@
           <div class="card">
             <div class="card-header" role="tab" id="headingOne1">
               <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne1" aria-expanded="true" aria-controls="collapseOne1">
-                <h5 class="mb-0"> Titulo 
+                <h5 class="mb-0"><?php echo $producao->titulo ?> 
                   <i class="fa fa-angle-down rotate-icon"></i>
                 </h5>
               </a>
@@ -31,13 +31,10 @@
             <!-- Card body -->
             <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordionEx">
               <div class="card-body">
-                Descrição
+                <?php echo $producao->descricao ?> 
                 <br>
                 <div class="media-div">
-                  <img src="<?php echo base_url('/assets/frontend/img/logo1.jpg'); ?>" />
-                  <img src="<?php echo base_url('/assets/frontend/img/logo2.jpg'); ?>" />
-                  <img src="<?php echo base_url('/assets/frontend/img/logo3.jpg'); ?>" />
-                  
+                  <img src="<?php echo base_url('/assets/frontend/img/logo1.jpg'); ?>" />                  
                   <button class="btn-galeria"> Abrir </button>
                 </div>
               </div>
